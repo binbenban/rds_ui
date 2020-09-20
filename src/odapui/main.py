76d5077abe109075bcc8ef5f2a17be8d4625b576
Bin Wang <bwang10@officeworks.com.au>
2020-09-20 11:25:11 +1000

    feature: refresh

--- src/odapui/main.py
+++ src/odapui/main.py
@@ -278,5 +278,10 @@ def refresh_all():
     return "done"
 
 
+@app.route("/ping")
+def ping():
+    return "pong"
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=3000, debug=True)
